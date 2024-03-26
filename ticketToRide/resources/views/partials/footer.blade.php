ad2d9a35b28f2d24234cfa0d5f701abebc864602
tsowden <tsowden.sup@gmail.com>
2024-03-26 09:43:17 +0100

    menu update + footer

--- ticketToRide/resources/views/partials/footer.blade.php
+++ ticketToRide/resources/views/partials/footer.blade.php
@@ -1,5 +1,10 @@
-<footer class="medieval-footer">
+<body>
+  <div class="site-content">
+  </div>
+
+  <footer class="medieval-footer">
     <div class="container">
         <p>&copy; {{ date('Y') }} Toniz' Production. All rights reserved.</p>
     </div>
-</footer>
+  </footer>
+</body>
--- ticketToRide/resources/views/partials/header.blade.php
+++ ticketToRide/resources/views/partials/header.blade.php
@@ -8,16 +8,18 @@
 
             <!-- visible si user authentifié -->
             @auth
-            <a href="{{ route('logout') }}" onclick="event.preventDefault(); document.getElementById('logout-form').submit();" class="nav-link gallery-2">
-                {{ Auth::user()->name }}: Se déconnecter
-            </a>
             <a href="{{ url('/play') }}" class="nav-link contact-2">Jouer</a>
             <a href="{{ url('/games') }}" class="nav-link contact-2">Parties</a>
             <a href="{{ url('/contact') }}" class="nav-link contact-2">Contact</a>
+            <a href="{{ route('logout') }}" onclick="event.preventDefault(); document.getElementById('logout-form').submit();" class="nav-link gallery-2">
+                {{ Auth::user()->name }}: Se déconnecter
+            </a>
+
             <!-- formulaire de déconnexion -->
             <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
                 @csrf
             </form>
+
             @else
             <!-- visibles si user pas connecté -->
             <a href="{{ route('login') }}" class="nav-link gallery-2">Se connecter</a>
