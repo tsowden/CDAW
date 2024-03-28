0c5e3e8b51f0b95f77b94a7537744ebcaf20da50
MHAYA Nizar <nizar.mhaya@gmail.com>
2024-03-28 12:59:18 +0100

    chat ok grace a inclusion script

--- ticketToRide/resources/views/chat.blade.php
+++ ticketToRide/resources/views/chat.blade.php
@@ -1,8 +1,10 @@
 @extends('layout')
 @section('content')
 
+<script defer src="{{ asset('js/script.js') }}"></script>
+
 <body id="message_body">
-    <div style="padding-top: 120px; padding-bottom: 120px" id="message-container"></div>
+    <div id="message-container"></div>
     <form id="send-container">
         <input type="text" id="message-input">
         <button type="submit" id="send-button"><i class="bi bi-arrow-up-circle-fill"></i></button>
