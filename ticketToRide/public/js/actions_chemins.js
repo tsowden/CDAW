11d3e487389ae872fec61ab419316d001cfa2b8d
MHAYA Nizar <nizar.mhaya@gmail.com>
2024-04-05 03:03:05 +0200

    ca marche

--- ticketToRide/public/js/actions_chemins.js
+++ ticketToRide/public/js/actions_chemins.js
@@ -32,87 +32,35 @@ function onClickButton(buttonId, username) {
     let chiffreId = "chiffre-" + buttonId.split("-")[1] + "-" + buttonId.split("-")[2];
     let chiffreElement = document.getElementById(chiffreId);
     let longueurChemin = parseInt(chiffreElement.innerText);
-    const bouton = document.getElementById(buttonId);
-    const backgroundColor = bouton.style.backgroundColor;
-    let couleurBouton = 'black';
-    if (backgroundColor === 'var(--color-chemin-noir)') {
-        // Si la couleur du bouton est noire, assignez 'black' à la variable couleurBouton
-        couleurBouton = 'black';
-    }
-    if (backgroundColor === 'var(--color-chemin-jaune)') {
-        // Si la couleur du bouton est noire, assignez 'black' à la variable couleurBouton
-        couleurBouton = 'yellow';
-    }
-    if (backgroundColor === 'var(--color-chemin-rouge)') {
-        // Si la couleur du bouton est noire, assignez 'black' à la variable couleurBouton
-        couleurBouton = 'red';
-    }
-    if (backgroundColor === 'var(--color-chemin-bleu)') {
-        // Si la couleur du bouton est noire, assignez 'black' à la variable couleurBouton
-        couleurBouton = 'blue';
-    }
-    if (backgroundColor === 'var(--color-chemin-rose)') {
-        // Si la couleur du bouton est noire, assignez 'black' à la variable couleurBouton
-        couleurBouton = 'violet';
-    }
-    if (backgroundColor === 'var(--color-chemin-vert)') {
-        // Si la couleur du bouton est noire, assignez 'black' à la variable couleurBouton
-        couleurBouton = 'green';
-    }
-    if (backgroundColor === 'var(--color-chemin-cyan)') {
-        // Si la couleur du bouton est noire, assignez 'black' à la variable couleurBouton
-        couleurBouton = 'cyan';
-    }
-    if (backgroundColor === 'var(--color-chemin-orange)') {
-        // Si la couleur du bouton est noire, assignez 'black' à la variable couleurBouton
-        couleurBouton = 'orange';
-    }
-    if (backgroundColor === 'var(--color-chemin-noir)') {
-        // Si la couleur du bouton est noire, assignez 'black' à la variable couleurBouton
-        couleurBouton = 'black';
-    }
-    if (backgroundColor === 'var(--color-chemin-gris)') {
-        // Si la couleur du bouton est noire, assignez 'black' à la variable couleurBouton
-        couleurBouton = 'gris';
-    }
+    document.getElementById(trajetId).style.backgroundColor = color;
+    document.getElementById(trajetId).style.height = "15px";
 
-
-    onPathClick(couleurBouton, longueurChemin)
-        .then(() => {
-            document.getElementById(trajetId).style.backgroundColor = color;
-            document.getElementById(trajetId).style.height = "15px";
-
-            // let pointsCell = document.getElementById('points-cell');
-            let tableCible;
-            if (username === userName) {
-                tableCible = document.querySelector('.table-opponent-auth');
-            }
-            else {
-                // Recherche de la table qui contient le bon nom d'utilisateur
-                const tablesAdversaires = document.querySelectorAll('.table-opponent');
-                tablesAdversaires.forEach(table => {
-                    const ths = table.querySelectorAll('th');
-                    ths.forEach(th => {
-                        if (th.textContent.trim() === username.trim()) {
-                            tableCible = table;
-                        }
-                    });
-                });
-            }
-            let pointsCell = tableCible.querySelector('#points-cell');
-            let wagonsCell = tableCible.querySelector('#wagons-cell');
-            tableCible.querySelector('th').style.backgroundColor = color;
-
-            let currentPoints = parseInt(pointsCell.innerText);
-            ajoutePoints(longueurChemin, pointsCell, currentPoints);
-            retireWagons(longueurChemin, wagonsCell);
-            bouton.disabled = true; // pour que le bouton ne soit cliquable qu'une seule fois
-        })
-        .catch((error) => {
-            // La promesse est rejetée, donc il y a eu une erreur dans la requête AJAX
-            // Vous pouvez traiter l'erreur ici
-            console.error("Le catch dans onClickButton s'est déclenché:", error);
+    // let pointsCell = document.getElementById('points-cell');
+    let tableCible;
+    if (username === userName) {
+        tableCible = document.querySelector('.table-opponent-auth');
+    }
+    else {
+        // Recherche de la table qui contient le bon nom d'utilisateur
+        const tablesAdversaires = document.querySelectorAll('.table-opponent');
+        tablesAdversaires.forEach(table => {
+            const ths = table.querySelectorAll('th');
+            ths.forEach(th => {
+                if (th.textContent.trim() === username.trim()) {
+                    tableCible = table;
+                }
+            });
         });
+    }
+    let pointsCell = tableCible.querySelector('#points-cell');
+    let wagonsCell = tableCible.querySelector('#wagons-cell');
+    tableCible.querySelector('th').style.backgroundColor = color;
+
+    let currentPoints = parseInt(pointsCell.innerText);
+    ajoutePoints(longueurChemin, pointsCell, currentPoints);
+    retireWagons(longueurChemin, wagonsCell);
+    bouton.disabled = true;
+
 
 }
 function retireWagons(longueurChemin, wagonsCell) {
@@ -164,7 +112,63 @@ let buttons = document.querySelectorAll(".line-button");
 // Ajouter un gestionnaire d'événements à chaque bouton
 buttons.forEach(button => {
     button.addEventListener("click", function () {
-        handleButtonClick(button.id);
+        const backgroundColor = button.style.backgroundColor;
+        let couleurBouton = 'black';
+        if (backgroundColor === 'var(--color-chemin-noir)') {
+            // Si la couleur du bouton est noire, assignez 'black' à la variable couleurBouton
+            couleurBouton = 'black';
+        }
+        if (backgroundColor === 'var(--color-chemin-jaune)') {
+            // Si la couleur du bouton est noire, assignez 'black' à la variable couleurBouton
+            couleurBouton = 'yellow';
+        }
+        if (backgroundColor === 'var(--color-chemin-rouge)') {
+            // Si la couleur du bouton est noire, assignez 'black' à la variable couleurBouton
+            couleurBouton = 'red';
+        }
+        if (backgroundColor === 'var(--color-chemin-bleu)') {
+            // Si la couleur du bouton est noire, assignez 'black' à la variable couleurBouton
+            couleurBouton = 'blue';
+        }
+        if (backgroundColor === 'var(--color-chemin-rose)') {
+            // Si la couleur du bouton est noire, assignez 'black' à la variable couleurBouton
+            couleurBouton = 'violet';
+        }
+        if (backgroundColor === 'var(--color-chemin-vert)') {
+            // Si la couleur du bouton est noire, assignez 'black' à la variable couleurBouton
+            couleurBouton = 'green';
+        }
+        if (backgroundColor === 'var(--color-chemin-cyan)') {
+            // Si la couleur du bouton est noire, assignez 'black' à la variable couleurBouton
+            couleurBouton = 'cyan';
+        }
+        if (backgroundColor === 'var(--color-chemin-orange)') {
+            // Si la couleur du bouton est noire, assignez 'black' à la variable couleurBouton
+            couleurBouton = 'orange';
+        }
+        if (backgroundColor === 'var(--color-chemin-noir)') {
+            // Si la couleur du bouton est noire, assignez 'black' à la variable couleurBouton
+            couleurBouton = 'black';
+        }
+        if (backgroundColor === 'var(--color-chemin-gris)') {
+            // Si la couleur du bouton est noire, assignez 'black' à la variable couleurBouton
+            couleurBouton = 'gris';
+        }
+        let chiffreId = "chiffre-" + button.id.split("-")[1] + "-" + button.id.split("-")[2];
+        let chiffreElement = document.getElementById(chiffreId);
+        let longueurChemin = parseInt(chiffreElement.innerText);
+
+        onPathClick(couleurBouton, longueurChemin)
+            .then(() => {
+                handleButtonClick(button.id); // pour que le bouton ne soit cliquable qu'une seule fois
+            })
+            .catch((error) => {
+                // La promesse est rejetée, donc il y a eu une erreur dans la requête AJAX
+                // Vous pouvez traiter l'erreur ici
+                console.error("Le catch dans onClickButton s'est déclenché:", error);
+            });
+
+
 
     });
     // onClickButton(button.id); // ceci est la fonction qui fait un changment de couleur mais on a plus besoin de l'appeler
